464a2d2096a9e6a53dbe369a2a00dcaa1fd3d683
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2024-03-05 02:17:21 +0900

    update signup-end view, styles

--- src/views/SignUpEndView.vue
+++ src/views/SignUpEndView.vue
@@ -1,7 +1,68 @@
 <template>
-  <div>signup End</div>
+  <div class="form-container">
+    <h3 class="form-title">
+      {{ signupInfo.username }}님, 회원가입이 완료되었습니다.
+    </h3>
+    <section class="signup-wrapper">
+      <div class="signup-info">
+        <strong>이메일 : </strong>
+        <span>{{ signupInfo.email }}</span>
+      </div>
+      <div class="signup-info">
+        <strong>주소 : </strong>
+        <span>{{ signupInfo.address }}</span>
+      </div>
+      <div class="signup-info">
+        <strong>연락처 : </strong>
+        <span>{{ dashPhoneNumber }}</span>
+      </div>
+    </section>
+    <div class="form-button-wrapper">
+      <button class="form-button next" @click="completeSignUp">홈으로</button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, reactive, watchEffect } from 'vue';
+import { useRouter } from 'vue-router';
+import { useSignUpStore } from '@/stores/signUp';
+import numberUtil from '@/utils/numberUtil';
 
-<style scoped></style>
+const router = useRouter();
+
+const signUp = useSignUpStore();
+
+const signupInfo = reactive({
+  username: '',
+  email: '',
+  address: '',
+  phone: '',
+});
+
+const dashPhoneNumber = computed(() => {
+  return numberUtil.addHyphenPhoneNumber(signupInfo.phone);
+});
+
+const completeSignUp = () => {
+  sessionStorage.removeItem('signup');
+  signUp.updateStep(1);
+
+  router.replace({
+    name: 'SignUp',
+  });
+};
+
+watchEffect(() => {
+  const signupSession: TypeSignup.SignupSession = JSON.parse(
+    sessionStorage.getItem('signup') as string,
+  );
+
+  if (signupSession) {
+    signupInfo.username = signupSession.username ?? '';
+    signupInfo.email = signupSession.email ?? '';
+    signupInfo.address = `${signupSession.address ?? ''} ${signupSession.addressDetail}`;
+    signupInfo.phone = signupSession.phone ?? '';
+  }
+});
+</script>
